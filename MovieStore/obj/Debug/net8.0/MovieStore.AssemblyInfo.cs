6b3a89126eca838e7eade6869c717775fbb5516f
mohamed abdallah <mohamedabdallahh26@gmail.com>
2024-11-25 00:03:38 +0200

    the previous commit added Also the full Movie functionality,Service,MVC, The project is complete right now

--- MovieStore/obj/Debug/net8.0/MovieStore.AssemblyInfo.cs
+++ MovieStore/obj/Debug/net8.0/MovieStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+812d4f417a395f22f7226bd88b7feedc7b3e2d24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f384f851f56a997cc38068f6d9dd6ffe94b88bc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
